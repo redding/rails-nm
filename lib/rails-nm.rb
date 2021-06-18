56cc23e953cfdcbf9893b814c5f74f0029aec72a
Kelly Redding <kelly@kellyredding.com>
2021-06-18 08:12:07 -0500

    gem generated with ggem

---
+++ lib/rails-nm.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails-nm/version"
+
+module RailsNm
+  # TODO: your code goes here...
+end
---
+++ test/helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# This file is automatically required when you run `assert`; put any test
+# helpers here.
+
+# Add the root dir to the load path.
+$LOAD_PATH.unshift(File.expand_path("../..", __FILE__))
+
+# Require pry for debugging (`binding.pry`).
+require "pry"
+
+require "test/support/factory"
+
+# TODO: put test helpers here...
